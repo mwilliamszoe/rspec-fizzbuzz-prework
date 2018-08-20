39086962405fabc26528fd029cb0192d6c3eb1b5
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-20 05:31:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(int)
   if int % 15 == 0
     "FizzBuzz"
-  elsif int % 3 == 0
-    "Fizz"
-    "Fizz"
-  else if int % 5 == 0
-    "Buzz"
-  else int % 3 != 0 && int % 5 != 0 && int % 15 != 0
-    nil
-  end
+  # elsif int % 3 == 0
+  #   "Fizz"
+  #   "Fizz"
+  # else if int % 5 == 0
+  #   "Buzz"
+  # else int % 3 != 0 && int % 5 != 0 && int % 15 != 0
+  #   nil
+  # end
 end
